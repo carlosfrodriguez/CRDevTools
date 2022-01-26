ce62f6157291849f7a658dffaaad314f87eb51fd
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2022-01-26 17:24:24 -0600

    Added Search and Delete commands for ITSM config items.

---
+++ lib/Console/Command/Dev/ITSMConfigItem/Delete.pm
@@ -0,0 +1,152 @@
+# --
+# Copyright (C) 2020 Carlos Rodriguez, https://github.com/carlosfrodriguez
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+# DO NOT USE THIS FILE ON PRODUCTION SYSTEMS!
+#
+# otrs is Copyright (C) 2001-2020 OTRS AG, http://otrs.com/
+# --
+
+package Console::Command::Dev::ITSMConfigItem::Delete;
+
+use strict;
+use warnings;
+
+use parent qw(Console::BaseCommand);
+
+our @ObjectDependencies = (
+    'Kernel::System::ITSMConfigItem',
+);
+
+sub Configure {
+    my ( $Self, %Param ) = @_;
+
+    $Self->Description('Delete one or more ITSM Config Items.');
+    $Self->AddOption(
+        Name        => 'name',
+        Description => "Specify the name of config items to be deleted e.g. *MyName*..",
+        Required    => 0,
+        HasValue    => 1,
+        ValueRegex  => qr/.*/smx,
+        Multiple    => 0,
+    );
+    $Self->AddOption(
+        Name        => 'id',
+        Description => 'Specify the email id of config items to be deleted e.g. *ITSMConfigItem*..',
+        Required    => 0,
+        HasValue    => 1,
+        ValueRegex  => qr/.*/smx,
+        Multiple    => 0,
+    );
+    $Self->AddOption(
+        Name        => 'id-range',
+        Description => "Specify a range of config items to be deleted. (e.g. 1..10)",
+        Required    => 0,
+        HasValue    => 1,
+        ValueRegex  => qr/\d+\.\.\d+/smx,
+        Multiple    => 0,
+    );
+
+    return;
+}
+
+sub PreRun {
+    my ( $Self, %Param ) = @_;
+
+    my $OptionsCounter = 0;
+    for my $Option (qw(name id id-range)) {
+        if ( $Self->GetOption($Option) ) {
+            $OptionsCounter++;
+        }
+    }
+
+    if ( $OptionsCounter > 1 ) {
+        die("Only one option (name or id) can be used at a time!\n");
+    }
+
+    return;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    $Self->Print("<yellow>Deleting ITSM config items...</yellow>\n");
+
+    my %SearchOptions;
+
+    my $Name = $Self->GetOption('name');
+
+    my @ItemsToDelete;
+    if ( $Self->GetOption('id') ) {
+        @ItemsToDelete = ( $Self->GetOption('id') );
+    }
+    elsif ( $Self->GetOption('id-range') ) {
+        if ( $Self->GetOption('id-range') =~ m{\A(\d+)\.\.(\d+)\z} ) {
+            @ItemsToDelete = ( $1 .. $2 );
+        }
+    }
+
+    # ITSMConfigItem name search
+    elsif ($Name) {
+        $SearchOptions{Name} = $Name;
+    }
+
+    no warnings 'once';    ## no critic
+    my $ITSMConfigItemObject = $Kernel::OM->Get('Kernel::System::ITSMConfigItem');
+
+    if (%SearchOptions) {
+
+        my $ItemIDs = $ITSMConfigItemObject->ConfigItemSearchExtended(
+            %SearchOptions,
+            Valid => 0,
+            Limit => 10_000,
+        );
+        @ItemsToDelete = sort @{$ItemIDs};
+    }
+
+    my $Failed;
+
+    ITEMID:
+    for my $ItemID (@ItemsToDelete) {
+
+        next ITEMID if !$ItemID;
+
+        my %Item = $ITSMConfigItemObject->ConfigItemGet(
+            ConfigItemID => $ItemID,
+        );
+
+        # Check if item exists.
+        if ( !%Item ) {
+            $Self->PrintError("The ITSM config item with ConfigItemID $ItemID does not exist!\n");
+            $Failed = 1;
+            next ITEMID;
+        }
+
+        # Delete config item.
+        my $Success = $ITSMConfigItemObject->ConfigItemDelete(
+            ConfigItemID => $ItemID,
+            UserID       => 1,
+        );
+
+        if ( !$Success ) {
+            $Self->PrintError("Can't delete ITSM config item $ItemID!\n");
+            $Failed = 1;
+            next ITEMID;
+        }
+
+        $Self->Print("  Deleted ITSM config item <yellow>$ItemID</yellow>\n");
+    }
+
+    if ($Failed) {
+        $Self->PrintError("Not all ITSM config items where deleted\n");
+        return $Self->ExitCodeError();
+    }
+
+    $Self->Print("<green>Done.</green>\n");
+    return $Self->ExitCodeOk();
+}
+
+1;
---
+++ lib/Console/Command/Dev/ITSMConfigItem/Search.pm
@@ -0,0 +1,120 @@
+# --
+# Copyright (C) 2020 Carlos Rodriguez, https://github.com/carlosfrodriguez
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+# DO NOT USE THIS FILE ON PRODUCTION SYSTEMS!
+#
+# otrs is Copyright (C) 2001-2020 OTRS AG, http://otrs.com/
+# --
+
+package Console::Command::Dev::ITSMConfigItem::Search;
+
+use strict;
+use warnings;
+
+use parent qw(Console::BaseCommand);
+
+our @ObjectDependencies = (
+    'Kernel::System::Ticket',
+    'Kernel::System::ITSMConfigItem',
+);
+
+sub Configure {
+    my ( $Self, %Param ) = @_;
+
+    $Self->Description('Search ITSMConfigItem in the system.');
+
+    $Self->AddOption(
+        Name        => 'name',
+        Description => "Search ITSMConfigItem with specified ITSM config item name e.g. *MyName*.",
+        Required    => 0,
+        HasValue    => 1,
+        ValueRegex  => qr/.*/smx,
+    );
+    $Self->AddOption(
+        Name        => 'number',
+        Description => "Search ITSMConfigItem with specified ITSM config item name e.g. 123.",
+        Required    => 0,
+        HasValue    => 1,
+        ValueRegex  => qr/.*/smx,
+    );
+
+    return;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    $Self->Print("<yellow>Listing ITSM config items...</yellow>\n");
+
+    my %SearchOptions;
+
+    # CustomerUser name search
+    if ( $Self->GetOption('name') ) {
+        $SearchOptions{Name} = $Self->GetOption('name');
+    }
+    if ( $Self->GetOption('number') ) {
+        $SearchOptions{Number} = $Self->GetOption('id');
+    }
+
+    no warnings 'once';    ## no critic
+    my $ITSMConfigItemObject = $Kernel::OM->Get('Kernel::System::ITSMConfigItem');
+
+    my $ItemIDs;
+
+    $ItemIDs = $ITSMConfigItemObject->ConfigItemSearchExtended(
+        %SearchOptions,
+        Limit => 10_000,
+    );
+
+    my @Items;
+
+    ITEM:
+    for my $ItemID ( @{ $ItemIDs || [] } ) {
+
+        next ITEM if !$ItemID;
+
+        my $Item = $ITSMConfigItemObject->ConfigItemGet(
+            ConfigItemID => $ItemID,
+        );
+        next ITEM if ref $Item ne 'HASH';
+
+        my $VersionRef = $ITSMConfigItemObject->VersionGet(
+            ConfigItemID => $ItemID,
+        ) || {};
+
+        # Prepare ITSMConfigItem information.
+        $Item->{Name}   = $VersionRef->{Name}   || $Item->{Name}         || '';
+        $Item->{Number} = $VersionRef->{Number} || $Item->{Number}       || '';
+        $Item->{ID}     = $VersionRef->{ID}     || $Item->{ConfigItemID} || '';
+
+        # Store item details.
+        push @Items, $Item,;
+    }
+
+    if ( !@Items ) {
+        $Self->Print("No ITSMConfigItems found\n");
+
+        $Self->Print("<green>Done.</green>\n");
+        return $Self->ExitCodeOk();
+    }
+
+    my $FormattedOutput = $Self->TableOutput(
+        TableData => {
+            Header => [ 'ID', 'Name', 'Number', ],
+            Body   => [ map { [ $_->{ID}, $_->{Name}, $_->{Number} ] } @Items ],
+        },
+        Indention => 2,
+        EvenOdd   => 'yellow',
+    );
+
+    $Self->Print("$FormattedOutput");
+
+    $Self->Print("<green>Done.</green>\n");
+    return $Self->ExitCodeOk();
+}
+
+1;
